cfd887623edfba8d7be691d13d07381c9f6c349a
iamzcr <iamzcr@gmail.com>
2016-04-05 23:50:54 +0800

    add bootstrap

--- api.py
+++ api.py
@@ -1,17 +1,26 @@
+#-*- coding: utf-8 -*-
+#encoding=utf-8
 import re
 import json
 import requests
+# list去重
+def uniq(arr):
+    arr2 = list(set(arr))
+    arr2.sort(key=arr.index)
+    return arr2
+
+default_timeout = 10
 class NetEase:
     def __init__(self):
         self.header = {
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip,deflate,sdch',
-            'Accept-Language': 'zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
             'Connection': 'keep-alive',
             'Content-Type': 'application/x-www-form-urlencoded',
             'Host': 'music.163.com',
-            'Referer': 'http://music.163.com/search/',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'
+            'Referer': 'http://music.163.com/',
+            'User-Agent': '	Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'
         }
         self.cookies = {
             'appver': '1.5.2'
@@ -33,16 +42,19 @@ class NetEase:
         connection.encoding = "UTF-8"
         connection = json.loads(connection.text)
         return connection
-    def top_songlist(self, offset=0, limit=100):
-        action = 'http://music.163.com/discover/toplist'
+    # 热门歌手 http://music.163.com/#/discover/artist/
+    def top_artists(self, offset=0, limit=100):
+        action = 'http://music.163.com/api/artist/top?offset=' + str(offset) + '&total=false&limit=' + str(limit)
         try:
-            connection = requests.get(action, headers=self.header, timeout=default_timeout)
-            connection.encoding = 'UTF-8'
-            songids = re.findall(r'/song\?id=(\d+)', connection.text)
-            if songids == []:
-                return []
-            # 去重
-            songids = uniq(songids)
-            return self.songs_detail(songids)
+            data = self.httpRequest('GET', action)
+            return data['artists']
+        except:
+            return []
+    # 歌手单曲
+    def artists(self, artist_id):
+        action = 'http://music.163.com/api/artist/' + str(artist_id)
+        try:
+            data = self.httpRequest('GET', action)
+            return data['hotSongs']
         except:
             return []
--- view.py
+++ view.py
@@ -1,13 +1,25 @@
 #-*- coding: utf-8 -*-
 #encoding=utf-8
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,request
 from api import NetEase
 app = Flask(__name__)
-@app.route('/index')
+@app.route('/')
 def index():
-    user = { 'nickname': 'Miguel' } # fake user
-    return render_template("index.html",title = 'Home',user = user)
+	net_music_list = NetEase.top_artists(NetEase())
+	print net_music_list
+	return render_template("index.html")
 
+@app.route('/singer')
+def singer():
+	singer_list = NetEase.top_artists(NetEase())
+	return render_template("singer.html",singer_list = singer_list)
+
+@app.route('/song_list',methods=['GET', 'POST'])
+def song_list():
+	singer_id = request.args.get('singer_id')
+	song_list = NetEase.artists(NetEase(),singer_id)
+	print  song_list
+	return render_template("song_list.html",song_list = song_list)
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
